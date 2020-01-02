5c3ff66f8488b854aa6e4c2ad0f4f39d3e8c82db
Leonardo Reis da Silva <33813822+reonardoleis@users.noreply.github.com>
2020-01-02 01:24:07 -0300

    v1.0

---
+++ src/content.js
@@ -0,0 +1,66 @@
+﻿
+
+var unlocked = 'waiting';
+var correct_pw = '';
+
+chrome.storage.local.get(function (data) {
+
+    correct_pw = data.password;
+
+});
+
+Verify();
+
+
+function Lock() {
+
+
+    chrome.storage.local.set({ key: 'false' }, function () {
+
+
+    });
+
+}
+
+function Unlock() {
+
+
+    chrome.storage.local.set({ key: 'true' }, function () {
+
+
+
+    });
+
+}
+
+function Verify() {
+
+    chrome.storage.local.get(function (data) {
+
+        console.log(data.key);
+        unlocked = data.key;
+
+    });
+
+}
+
+
+setTimeout(() => {
+console.log(unlocked)
+while (unlocked != "true") {
+    let pass = window.prompt("Type the password:");
+    console.log(unlocked);
+    if (pass == correct_pw) {
+        Unlock();
+        break;
+    }
+    Verify();
+}
+
+}, 100);
+
+
+
+
+
+
---
+++ src/options.js
@@ -0,0 +1,27 @@
+﻿function Lock() {
+
+
+    chrome.storage.local.set({ key: 'false' }, function () {
+
+    
+
+    });
+
+}
+
+function SetPassword() {
+
+    let new_pw = document.getElementById('password').value;
+
+    chrome.storage.local.set({ password: new_pw }, function () {
+
+      
+
+    });
+
+}
+
+
+
+document.getElementById('lock').onclick = Lock;
+document.getElementById('set').onclick = SetPassword;
